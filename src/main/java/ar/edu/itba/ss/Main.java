eb01710a0a56b16059b343b7a438df6e43e946dd
marquitos.eth <mdedeu@itba.edu.ar>
2024-03-17 18:21:19 -0300

    misc: file structure

---
+++ src/main/java/ar/edu/itba/ss/Main.java
@@ -0,0 +1,12 @@
+package main.java.ar.edu.itba.ss;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //generar dynamic particle file
+        //Leer particles Dynamic y static
+        //Implementación OffLatice
+        //ver que puedo reciclar del cimWithContour del TP1
+        //archivos output
+    }
+}
